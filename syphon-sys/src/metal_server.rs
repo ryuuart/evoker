6271a283351590a71c8e786ab274ced5722df206
Long Nguyen <me@long-nguyen.dev>
2025-01-23 21:37:31 -0600

    feat: update class implementation and objc2 interface

--- syphon-sys/src/metal_server.rs
+++ syphon-sys/src/metal_server.rs
@@ -1,38 +1,36 @@
 use crate::server_base::*;
 use objc2::{
-    extern_class, extern_methods, mutability,
+    extern_class, extern_methods,
     rc::{Allocated, Retained},
     runtime::ProtocolObject,
-    ClassType,
 };
 use objc2_foundation::{NSDictionary, NSRect, NSString};
 use objc2_metal::{MTLCommandBuffer, MTLCreateSystemDefaultDevice, MTLDevice, MTLTexture};
 
 extern_class!(
+    #[unsafe(super(SyphonServerBase))]
     pub struct SyphonMetalServer;
-
-    unsafe impl ClassType for SyphonMetalServer {
-        type Super = SyphonServerBase;
-        type Mutability = mutability::InteriorMutable;
-    }
 );
 
-extern_methods!(
-    unsafe impl SyphonMetalServer {
-        #[method_id(initWithName:device:options:)]
+impl SyphonMetalServer {
+    extern_methods!(
+        #[unsafe(method(alloc))]
+        fn alloc() -> Allocated<Self>;
+
         /// Creates a new server with the specified human-readable name (which need not be unique) for a
         /// [MTLDevice] and given options. The server will be started immediately. Init may fail and
         /// return nil if the server could not be started.
+        #[unsafe(method(initWithName:device:options:))]
         fn init_with_name(
             this: Allocated<Self>,
             name: &NSString,
-            device: *mut ProtocolObject<dyn MTLDevice>,
+            device: *const ProtocolObject<dyn MTLDevice>,
             options: Option<&NSDictionary>,
         ) -> Retained<Self>;
 
-        #[method(publishFrameTexture:onCommandBuffer:imageRegion:flipped:)]
         /// Publishes the part of the texture described in region of the texture to clients.
         /// The texture is copied and can be safely modified once this method has returned.
+        #[unsafe(method(publishFrameTexture:onCommandBuffer:imageRegion:flipped:))]
         pub fn publish_frame_texture(
             &self,
             texture_to_publish: *mut ProtocolObject<dyn MTLTexture>,
@@ -41,22 +39,20 @@ extern_methods!(
             is_flipped: bool,
         );
 
-        #[method(stop)]
+        #[unsafe(method(stop))]
         /// Stops the server instance. Use of this method is optional and releasing all
         /// references to the server has the same effect.
         pub fn stop(&self);
-    }
-);
+    );
 
-impl SyphonMetalServer {
     /// Convenience method to create a new server in one go
     pub fn new(name: &str) -> Retained<Self> {
         let instance = SyphonMetalServer::alloc();
         let name = NSString::from_str(name);
-        let device = unsafe { MTLCreateSystemDefaultDevice() };
+        let device = MTLCreateSystemDefaultDevice().expect("Couldn't use Metal GPU device.");
         let options = None;
 
-        Self::init_with_name(instance, &name, device, options)
+        Self::init_with_name(instance, &name, Retained::as_ptr(&device), options)
     }
 
     /// Useful for when we need to control the pipeline outside with the device
--- syphon-sys/src/server_base.rs
+++ syphon-sys/src/server_base.rs
@@ -1,10 +1,6 @@
-use objc2::{extern_class, mutability, runtime::NSObject, ClassType};
+use objc2::{extern_class, runtime::NSObject};
 
 extern_class!(
+    #[unsafe(super(NSObject))]
     pub struct SyphonServerBase;
-
-    unsafe impl ClassType for SyphonServerBase {
-        type Super = NSObject;
-        type Mutability = mutability::InteriorMutable;
-    }
 );
